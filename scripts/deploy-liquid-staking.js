693eea32383a847d513361430a31f4ca116c1beb
Cryptarch <cryptarchpetro@gmail.com>
2025-08-28 11:48:42 +0200

    Create deploy-liquid-staking.js

---
+++ scripts/deploy-liquid-staking.js
@@ -0,0 +1,53 @@
+// base-defi-liquid-staking/scripts/deploy.js
+const { ethers } = require("hardhat");
+
+async function main() {
+  console.log("Deploying Base DeFi Liquid Staking...");
+  
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  // Деплой токенов
+  const RewardToken = await ethers.getContractFactory("ERC20Token");
+  const rewardToken = await RewardToken.deploy("Reward Token", "REWARD");
+  await rewardToken.deployed();
+  
+  const StakingToken = await ethers.getContractFactory("ERC20Token");
+  const stakingToken = await StakingToken.deploy("Staking Token", "STAKE");
+  await stakingToken.deployed();
+
+  // Деплой Liquid Staking контракта
+  const LiquidStaking = await ethers.getContractFactory("LiquidStakingV3");
+  const staking = await LiquidStaking.deploy(
+    rewardToken.address,
+    stakingToken.address,
+    ethers.utils.parseEther("100"), // 100 tokens per second
+    ethers.utils.parseEther("1000"), // 1000 minimum stake
+    ethers.utils.parseEther("100000") // 100000 maximum stake
+  );
+
+  await staking.deployed();
+
+  console.log("Base DeFi Liquid Staking deployed to:", staking.address);
+  console.log("Reward Token deployed to:", rewardToken.address);
+  console.log("Staking Token deployed to:", stakingToken.address);
+  
+  // Сохраняем адреса
+  const fs = require("fs");
+  const data = {
+    staking: staking.address,
+    rewardToken: rewardToken.address,
+    stakingToken: stakingToken.address,
+    owner: deployer.address
+  };
+  
+  fs.writeFileSync("./config/deployment.json", JSON.stringify(data, null, 2));
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
