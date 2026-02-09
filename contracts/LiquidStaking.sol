838af03bf023d31cd95d6df57df320ffabaed3cb
Cryptarch <cryptarchpetro@gmail.com>
2026-02-09 09:04:50 +0100

    Update LiquidStaking.sol

--- contracts/LiquidStaking.sol
+++ contracts/LiquidStaking.sol
@@ -12,7 +12,7 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import "./StakedToken.sol";
+import "./StakedToken.sol"; 
 
 contract BaseLiquidStaking is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
