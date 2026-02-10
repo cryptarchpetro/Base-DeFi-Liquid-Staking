fa9e22f833abf3a8f68b602def03f53737f56110
Cryptarch <cryptarchpetro@gmail.com>
2026-02-10 15:39:11 +0100

    Update LiquidStaking.sol

--- contracts/LiquidStaking.sol
+++ contracts/LiquidStaking.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20; 
 
-/*
-  Educational liquid staking:
+
   - deposit TOKEN -> mint stTOKEN 1:1
   - request redeem (cooldown) -> claim underlying
 */
