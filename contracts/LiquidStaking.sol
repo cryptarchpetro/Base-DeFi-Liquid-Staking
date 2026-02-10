42c2af572861780cfceb0b534f06a0dc1b2a33a8
Cryptarch <cryptarchpetro@gmail.com>
2026-02-10 15:39:26 +0100

    Update LiquidStaking.sol

--- contracts/LiquidStaking.sol
+++ contracts/LiquidStaking.sol
@@ -2,10 +2,6 @@
 pragma solidity ^0.8.20; 
 
 
-
-  - request redeem (cooldown) -> claim underlying
-*/
-
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
